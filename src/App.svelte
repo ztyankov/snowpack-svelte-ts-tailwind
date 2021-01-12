956fb34b3f5947aa3f13bd75e3f1311bafb1957b
Zdravko Tyankov <ztyankov@gmail.com>
2021-01-13 01:50:07 +0200

    Update Card Component Style & Customize TW Theme

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,28 @@
     import { onMount } from "svelte";
 
     let count: number = 0;
+    const data = [
+        {
+            id: 1,
+            name: "Plain Pasta",
+            cuisine: "Italian",
+            ingredients: [
+                "spaghetti",
+                "tomato sauce",
+                "tomato sauce",
+                "tomato sauce",
+            ],
+            spices: ["oregano", "salt", "hot sauce"],
+            type: ["Brunch", "Lunch", "Dinner"],
+            cookingTime: {
+                min: 25,
+                max: 30,
+            },
+            tags: ["quick", "easy", "pasta"],
+        },
+    ];
+    const meal = data[0];
+
     onMount(() => {
         const interval = setInterval(() => count++, 1000);
         return () => {
@@ -13,66 +35,56 @@
 <style>
     :global(body) {
         margin: 0;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-    div code {
-        background: #0002;
-        padding: 4px 8px;
-        border-radius: 4px;
-    }
-    div p {
-        margin: 0.4rem;
+        /* font-family: Arial, Helvetica, sans-serif; */
     }
 
-    div header {
-        background-color: #f9f6f6;
-        color: #333;
-        min-height: 100vh;
-        font-size: calc(10px + 2vmin);
-    }
-    div a {
-        color: #ff3e00;
-    }
-    header img {
-        height: 36vmin;
-        margin-bottom: 3rem;
-        animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-    }
-    @keyframes App-logo-spin {
-        from {
-            transform: scale(1);
-        }
-        to {
-            transform: scale(1.06);
-        }
+    .card-label {
+        @apply text-lg text-blue-800 font-semibold;
     }
 </style>
+
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
+</svelte:head>
 
 <div class="w-64 py-2 px-2 mx-auto bg-white rounded-xl shadow-md my-4">
     <p class="text-center space-y-2">
         Page has been open for
-        <code>{count}</code>
+        <code class="bg-gray-200 px-2">{count}</code>
         seconds.
     </p>
 </div>
 
 <div
     class="grid grid-rows-2 grid-flow-col gap-2 w-80 mx-auto bg-white rounded-xl shadow-md">
-    <div class="min-w-full sm:mx-0 sm:flex-shrink-0">
+    <div class="relative min-w-full sm:mx-0 sm:flex-shrink-0 shadow-md">
         <img src="https://picsum.photos/id/163/360/200" alt="Eating Location" />
-        <p
-            class="text-lg text-black font-semibold"
-            style="position: absolute; bottom: -20px">
-            Erin Lindford
-        </p>
+        <div class="absolute bottom-1 left-1 bg-black bg-opacity-40 px-2 py-1">
+            <p class="text-lg text-white font-semibold">{meal.name}</p>
+            <p class="text-md text-white font-thin italic">{meal.cuisine}</p>
+        </div>
     </div>
 
-    <div class="text-left space-y-2 py-4 px-4 sm:text-left">
-        <div class="space-y-0.5">
-            <p class="text-lg text-black font-semibold">Erin Lindford</p>
-            <p class="text-gray-500 font-medium">Product Engineer</p>
+    <div class="relative text-left space-y-2 py-4 px-4 sm:text-left">
+        <div class="space-y-1">
+            <p class="text-black">
+                <span class="card-label">Prep time</span>:
+                {meal.cookingTime.min}
+                -
+                {meal.cookingTime.max}
+                min
+            </p>
+            <p>
+                <span class="card-label">Ingerdients</span>:
+                {meal.ingredients.join(', ')}
+            </p>
         </div>
         <button
-            class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+            class="absolute left-2 bottom-2 px-4 pt-1.5 pb-1 text-md text-svelte-800 font-md rounded-lg hover:text-white hover:bg-svelte-200 hover:border-transparent focus:outline-none focus:bg-svelte-400 focus:text-white uppercase">Select</button>
     </div>
 </div>
