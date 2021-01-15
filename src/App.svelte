fe23d554fdaa725334248b16068d656554caf5ae
Zdravko Tyankov <ztyankov@gmail.com>
2021-01-16 01:03:42 +0200

    Create Spinning Animation for Random Options Selector
    - Create a custom Svelte animation by using elasticOut and some custom
    colors logic
    - Continuously change the content that's annimated before settling for
    the real selection
    - Create a primary button style that applies the current styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,58 @@
 <script lang="typescript">
     import { onMount } from "svelte";
+	import { fade } from 'svelte/transition';
+	import { elasticOut } from 'svelte/easing';
+
+    const animationDuration = 8000;
+    let visible = false;
+    let showIntro = true;
+    let selected = `Let's see what we have in store for you!`;
+    const options = ['Cereal with banana', 'Domino\'s Pizza', 'Chilli', 'Pasta Bolonese', 'Gin and Tonic', 'Gormet Burgers', 'Kebapcheta and Kiufteta'];
+
+    function selectAtRandom() {
+        visible = !visible;
+        showIntro = false;
+
+        const pickIsIn = getRandomInteger(0, options.length);
+        const carouselOptions = [
+            ...options.slice(0, pickIsIn),
+            ...options.slice(pickIsIn + 1)
+        ]
+
+        console.log(options[pickIsIn], carouselOptions);
+        const changeSelectionInterval = animationDuration / 10;
+
+        const interval = setInterval(() => {
+            const randomIndex = getRandomInteger(0, carouselOptions.length);
+            selected = carouselOptions[randomIndex];
+        }, changeSelectionInterval);
+
+        setTimeout(() => {
+            selected = options[pickIsIn];
+            clearInterval(interval);
+        }, animationDuration - changeSelectionInterval - 10);
+    }
+
+    function getRandomInteger(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+
+	function spin(node, { duration }) {
+		return {
+			duration,
+			css: (t: number) => {
+				const eased = elasticOut(t);
+
+				return `
+					transform: scale(${eased}) rotate(${eased * 1080}deg);
+					color: hsl(
+						${~~(t * 360)},
+						${Math.min(100, 1000 - 1000 * t)}%,
+						${Math.min(50, 500 - 500 * t)}%
+					);`
+			}
+		};
+	}
 
     let count: number = 0;
     const data = [
@@ -41,6 +94,10 @@
     .card-label {
         @apply text-lg text-blue-800 font-semibold;
     }
+
+    button.primary {
+        @apply px-4 pt-1.5 pb-1 text-lg text-svelte-800 rounded-lg hover:text-white hover:bg-svelte-200 hover:border-transparent focus:outline-none focus:bg-svelte-400 focus:text-white uppercase;
+    }
 </style>
 
 <svelte:head>
@@ -58,6 +115,21 @@
         <code class="bg-gray-200 px-2">{count}</code>
         seconds.
     </p>
+</div>
+
+
+<div class="flex relative w-80 h-80 mx-auto my-4 rounded-xl shadow-md">
+    <button class="primary self-start mx-auto" on:click={selectAtRandom}>Pick my next meal</button>
+
+    {#if visible}
+        <div class="absolute left-1/3 top-1/2 w-full" in:spin="{{duration: animationDuration}}" out:fade>
+            <span class="absolute text-xl">{selected}</span>
+        </div>
+    {:else if showIntro}
+        <div class="absolute left-auto top-1/2 w-full" out:fade>
+            <span class="absolute text-lg p-4">`Let's see what we have in store for you!`</span>
+        </div>
+    {/if}
 </div>
 
 <div
@@ -84,7 +156,6 @@
                 {meal.ingredients.join(', ')}
             </p>
         </div>
-        <button
-            class="absolute left-2 bottom-2 px-4 pt-1.5 pb-1 text-md text-svelte-800 font-md rounded-lg hover:text-white hover:bg-svelte-200 hover:border-transparent focus:outline-none focus:bg-svelte-400 focus:text-white uppercase">Select</button>
+        <button class="primary absolute left-2 bottom-2">Select</button>
     </div>
 </div>
